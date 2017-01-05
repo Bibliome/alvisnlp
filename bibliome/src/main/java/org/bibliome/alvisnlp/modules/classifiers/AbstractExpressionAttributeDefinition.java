1d62f3545dfdf5f052b1db567ee0e5e6f1c6a918
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-01-05 09:22:19 +0100

    updated commend header in Java sources

--- bibliome/src/main/java/org/bibliome/alvisnlp/modules/classifiers/AbstractExpressionAttributeDefinition.java
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/classifiers/AbstractExpressionAttributeDefinition.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Institut National de la Recherche Agronomique
+Copyright 2016, 2017 Institut National de la Recherche Agronomique
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 
 package org.bibliome.alvisnlp.modules.classifiers;
 
