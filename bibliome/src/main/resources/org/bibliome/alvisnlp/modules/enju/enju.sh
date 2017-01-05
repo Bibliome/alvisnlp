ce1dac365a8ede17f0d19dec0443e0eaad09ee0b
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-01-05 09:44:36 +0100

    updated commend header in resources

--- bibliome/src/main/resources/org/bibliome/alvisnlp/modules/enju/enju.sh
+++ bibliome/src/main/resources/org/bibliome/alvisnlp/modules/enju/enju.sh
@@ -1,4 +1,4 @@
-# Copyright 2016 Institut National de la Recherche Agronomique
+# Copyright 2016, 2017 Institut National de la Recherche Agronomique
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,5 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 $ENJU_BIN -nt -so $ENJU_MODEL -W $ENJU_LENGTH $ENJU_N <$ENJU_IN >$ENJU_OUT
