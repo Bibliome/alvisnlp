b6abbcdf49e9ff8ceced70c1654b6db07f998567
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-03-07 14:13:34 +0100

    TEESPredict#iteratorSnippet()

--- bibliome/src/main/java/org/bibliome/alvisnlp/modules/tees/TEESPredict.java
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/tees/TEESPredict.java
@@ -1,14 +1,25 @@
 package org.bibliome.alvisnlp.modules.tees;
 
-import org.bibliome.alvisnlp.modules.CorpusModule;
-import org.bibliome.alvisnlp.modules.ResolvedObjects;
+import java.util.logging.Logger;
 
+import org.bibliome.alvisnlp.modules.SectionModule;
+import org.bibliome.alvisnlp.modules.SectionModule.SectionResolvedObjects;
+import org.bibliome.util.Iterators;
+
+import alvisnlp.corpus.Annotation;
 import alvisnlp.corpus.Corpus;
+import alvisnlp.corpus.DefaultNames;
+import alvisnlp.corpus.Document;
+import alvisnlp.corpus.Layer;
+import alvisnlp.corpus.Section;
+import alvisnlp.corpus.expressions.EvaluationContext;
 import alvisnlp.corpus.expressions.ResolverException;
 import alvisnlp.module.ModuleException;
 import alvisnlp.module.ProcessingContext;
 
-public class TEESPredict extends CorpusModule<ResolvedObjects> {
+public class TEESPredict extends SectionModule<SectionResolvedObjects> {
+	private String tokenLayerName = DefaultNames.getWordLayer();
+	private String sentenceLayerName = DefaultNames.getSentenceLayer();
 
 	@Override
 	public void process(ProcessingContext<Corpus> ctx, Corpus corpus) throws ModuleException {
@@ -17,9 +28,65 @@ public class TEESPredict extends CorpusModule<ResolvedObjects> {
 	}
 
 	@Override
-	protected ResolvedObjects createResolvedObjects(ProcessingContext<Corpus> ctx) throws ResolverException {
-		// TODO Auto-generated method stub
-		return new ResolvedObjects(ctx, this);
+	protected SectionResolvedObjects createResolvedObjects(ProcessingContext<Corpus> ctx) throws ResolverException {
+		return new SectionResolvedObjects(ctx, this);
+	}
+	
+	@Override
+	protected String[] addLayersToSectionFilter() {
+		return new String[] {
+				tokenLayerName,
+				sentenceLayerName
+		};
 	}
 
+	@Override
+	protected String[] addFeaturesToSectionFilter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void iteratorSnippet(ProcessingContext<Corpus> ctx, Corpus corpus) {
+		Logger logger = getLogger(ctx);
+		EvaluationContext evalCtx = new EvaluationContext(logger);
+
+		// iteration des documents du corpus
+		for (Document doc : Iterators.loop(documentIterator(evalCtx, corpus))) {
+			// faire qqch avec doc, par ex
+			doc.getId();
+			doc.getLastFeature("DOCFEATUREKEY");
+			// iteration des sections du document
+			for (Section sec : Iterators.loop(sectionIterator(evalCtx, doc))) {
+				// faire qqch avec sec, par ex
+				sec.getName();
+				sec.getLastFeature("SECFEATUREKEY");
+				sec.getDocument();
+				// iteration des annotations d'un layer dans une section
+				Layer layer = sec.ensureLayer("LAYERNAME");
+				for (Annotation a : layer) {
+					// faire qqch avec a, par ex
+					a.getStart();
+					a.getEnd();
+					a.getLength();
+					a.getLastFeature("ANNOTATIONFEATUREKEY");
+					a.getSection();
+				}
+				
+				// iteration des sentences dans une section
+				for (Layer sentLayer : sec.getSentences(tokenLayerName, sentenceLayerName)) {
+					Annotation sent = sentLayer.getSentenceAnnotation();
+					// faire qqch avec sent
+					// iteration des mots dans une sentence
+					for (Annotation token : sentLayer) {
+						// faire qqch avec token
+					}
+				}
+			}
+		}
+		
+		// on peut iterer les sections sans passer par les documents
+		for (Section sec : Iterators.loop(sectionIterator(evalCtx, corpus))) {
+			//
+		}
+	}
 }
--- bibliome/src/main/java/org/bibliome/alvisnlp/modules/tees/TEESTrain.java
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/tees/TEESTrain.java
@@ -1,14 +1,14 @@
 package org.bibliome.alvisnlp.modules.tees;
 
-import org.bibliome.alvisnlp.modules.CorpusModule;
-import org.bibliome.alvisnlp.modules.ResolvedObjects;
+import org.bibliome.alvisnlp.modules.SectionModule;
+import org.bibliome.alvisnlp.modules.SectionModule.SectionResolvedObjects;
 
 import alvisnlp.corpus.Corpus;
 import alvisnlp.corpus.expressions.ResolverException;
 import alvisnlp.module.ModuleException;
 import alvisnlp.module.ProcessingContext;
 
-public class TEESTrain  extends CorpusModule<ResolvedObjects>  {
+public class TEESTrain  extends SectionModule<SectionResolvedObjects>  {
 
 	@Override
 	public void process(ProcessingContext<Corpus> ctx, Corpus corpus) throws ModuleException {
@@ -17,7 +17,18 @@ public class TEESTrain  extends CorpusModule<ResolvedObjects>  {
 	}
 
 	@Override
-	protected ResolvedObjects createResolvedObjects(ProcessingContext<Corpus> ctx) throws ResolverException {
+	protected SectionResolvedObjects createResolvedObjects(ProcessingContext<Corpus> ctx) throws ResolverException {
+		return new SectionResolvedObjects(ctx, this);
+	}
+
+	@Override
+	protected String[] addLayersToSectionFilter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] addFeaturesToSectionFilter() {
 		// TODO Auto-generated method stub
 		return null;
 	}
