47a27980b951650dd16339a107e4aa49b05a1ddb
mouhamadou ba <mandiayba@gmail.com>
2017-04-18 14:45:28 +0200

    Update train.sh

--- bibliome/src/main/resources/org/bibliome/alvisnlp/modules/tees/train.sh
+++ bibliome/src/main/resources/org/bibliome/alvisnlp/modules/tees/train.sh
@@ -18,4 +18,4 @@
 python $TEES_PRE_EXE --input  $TEES_TRAIN_IN --output $TEES_TRAIN_OUT --omitSteps $OMITSTEPS --debug true
 python $TEES_PRE_EXE --input  $TEES_DEV_IN --output $TEES_DEV_OUT --omitSteps $OMITSTEPS --debug true
 python $TEES_PRE_EXE --input  $TEES_TEST_IN --output $TEES_TEST_OUT --omitSteps $OMITSTEPS --debug true
-} && python $TEES_TRAIN_EXE --trainFile $TEES_TRAIN_OUT --develFile $TEES_DEV_OUT --testFile $TEES_TEST_OUT -o $WORKDIR --debug true && cp -r $WORKDIR/model-test $MODEL
+} && python $TEES_TRAIN_EXE --trainFile $TEES_TRAIN_OUT --develFile $TEES_DEV_OUT --testFile $TEES_TEST_OUT -o $WORKDIR --debug true && zip -r model $WORKDIR/model-test/* && cp -r $WORKDIR/model.zip $MODEL && 
