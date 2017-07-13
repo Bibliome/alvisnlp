874e8fdb3c3b8a6eff89307a7dee6785e300f5de
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-13 19:36:52 +0200

    deprecated alias FileMapper2

---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/mapper/FileMapper2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.mapper;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=FileMapper.class)
+public class FileMapper2 extends FileMapper {
+
+}
