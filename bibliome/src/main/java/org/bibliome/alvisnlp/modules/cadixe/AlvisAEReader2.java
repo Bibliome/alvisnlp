54372bc7fdf7afb93c00d98d6f74fa947c28c983
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-13 19:32:02 +0200

    deprecated alias AlvisAEReader2

---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/cadixe/AlvisAEReader2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.cadixe;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=AlvisAEReader.class)
+public abstract class AlvisAEReader2 extends AlvisAEReader {
+
+}
