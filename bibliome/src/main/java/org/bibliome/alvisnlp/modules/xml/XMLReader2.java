ed91e0d155c5b7d638a06efadad86b13116982dc
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-13 19:26:29 +0200

    deprecated XMLReader2 and XMLWriter2 aliases

---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/xml/XMLReader2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.xml;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=XMLReader.class)
+public class XMLReader2 {
+
+}
---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/xml/XMLWriter2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.xml;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=XMLWriter.class)
+public class XMLWriter2 {
+
+}
