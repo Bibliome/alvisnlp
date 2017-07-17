33be0feb52aedad80ca8e03787ac1daa777370e1
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-17 08:51:19 +0200

    deprecated SimpleProjector2 and ElementProjector2

---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/trie/ElementProjector2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.trie;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=ElementProjector.class)
+public abstract class ElementProjector2 extends ElementProjector {
+
+}
---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/trie/SimpleProjector2.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.trie;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=SimpleProjector.class)
+public abstract class SimpleProjector2 extends SimpleProjector {
+
+}
