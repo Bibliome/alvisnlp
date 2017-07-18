b7a7cff649206e90c59d89499735a912867c5276
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-18 19:25:32 +0200

    deprecated module ExportCadixeJSON

---
+++ bibliome/src/main/java/org/bibliome/alvisnlp/modules/alvisae/ExportCadixeJSON.java
@@ -0,0 +1,8 @@
+package org.bibliome.alvisnlp.modules.alvisae;
+
+import alvisnlp.module.lib.AlvisNLPModule;
+
+@AlvisNLPModule(obsoleteUseInstead=AlvisAEWriter.class)
+public class ExportCadixeJSON extends AlvisAEWriter {
+
+}
