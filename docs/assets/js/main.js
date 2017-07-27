aa673729667268a0390bfdb087d4732ebd9584bd
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-07-27 22:53:14 +0200

    only insert a ToC if needed

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -13,18 +13,21 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(document).ready(function(){
-  $("h2, h3, h4, h5").each(function(){
-    $("nav ul").append("<li class='tag-h tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-  });
+    if ($("h2, h3, h4, h5").length) {
+	$("nav").prepend("<div class='toc'>Table of Contents</div>");
+	$("h2, h3, h4, h5").each(function(){
+	    $("nav ul").append("<li class='tag-h tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+	    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+	});
 
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();
-  });
+	$("nav ul li").on("click", "a", function(event) {
+	    var position = $($(this).attr("href")).offset().top;
+	    $("html, body").animate({scrollTop: position}, 400);
+	    $("nav ul li a").parent().removeClass("active");
+	    $(this).parent().addClass("active");
+	    event.preventDefault();
+	});
+    }
 
   sectionHeight();
 
