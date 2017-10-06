c9c4261263329255cbc5553ff082a3ec1927d17c
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2017-10-06 12:12:35 +0200

    Chemspot as external software

---
+++ alvisnlp-bibliome/src/main/java/org/bibliome/alvisnlp/modules/chemspot/Chemspot.java
@@ -0,0 +1,328 @@
+package org.bibliome.alvisnlp.modules.chemspot;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.bibliome.alvisnlp.modules.SectionModule;
+import org.bibliome.alvisnlp.modules.SectionModule.SectionResolvedObjects;
+import org.bibliome.util.Iterators;
+import org.bibliome.util.files.InputDirectory;
+
+import alvisnlp.corpus.Annotation;
+import alvisnlp.corpus.Corpus;
+import alvisnlp.corpus.Layer;
+import alvisnlp.corpus.NameType;
+import alvisnlp.corpus.Section;
+import alvisnlp.corpus.creators.AnnotationCreator;
+import alvisnlp.corpus.expressions.EvaluationContext;
+import alvisnlp.corpus.expressions.ResolverException;
+import alvisnlp.module.ModuleException;
+import alvisnlp.module.ProcessingContext;
+import alvisnlp.module.lib.AlvisNLPModule;
+import alvisnlp.module.lib.Param;
+
+@AlvisNLPModule(beta=true)
+public abstract class Chemspot extends SectionModule<SectionResolvedObjects> implements AnnotationCreator {
+	private InputDirectory javaHome = new InputDirectory(System.getenv("JAVA_HOME"));
+	private InputDirectory chemspotDir;
+	private Boolean noDict = false;
+	private String targetLayerName = "chemspot";
+	private String fdaDateFeatureName = "FDA";
+	private String fdaFeatureName = "FDA_DATE";
+	private String meshFeatureName = "MESH";
+	private String kegdFeatureName = "KEGD";
+	private String keggFeatureName = "KEGG";
+	private String hmdbFeatureName = "HMDB";
+	private String drugFeatureName = "DRUG";
+	private String inchFeatureName = "INCH";
+	private String pubsFeatureName = "PUBS";
+	private String pubcFeatureName = "PUBC";
+	private String casFeatureName = "CAS";
+	private String chebFeatureName = "CHEB";
+	private String chidFeatureName = "CHID";
+	private String chemTypeFeatureName = "chem-type";
+
+	@Override
+	public void process(ProcessingContext<Corpus> ctx, Corpus corpus) throws ModuleException {
+		try {
+			File tmpDir = getTempDir(ctx);
+			Logger logger = getLogger(ctx);
+			EvaluationContext evalCtx = new EvaluationContext(logger);
+			ChemspotExternal<Corpus> external = new ChemspotExternal<>(ctx, this, javaHome, chemspotDir, noDict, tmpDir);
+			for (Section sec : Iterators.loop(sectionIterator(evalCtx, corpus))) {
+				String name = sec.getFileName();
+				String content = sec.getContents();
+				external.addInput(name, content);
+			}
+			callExternal(ctx, "run-chemspot", external);
+			ChemspotFileLines<Layer,Annotation> chemspotFileLines = new CorpusChemspotFileLines();
+			for (Section sec : Iterators.loop(sectionIterator(evalCtx, corpus))) {
+				String name = sec.getFileName();
+				Layer layer = sec.ensureLayer(targetLayerName);
+				external.readOutput(chemspotFileLines, layer, name);
+			}
+		}
+		catch (IOException e) {
+			rethrow(e);
+		}
+	}
+	
+	private class CorpusChemspotFileLines extends ChemspotFileLines<Layer,Annotation> {
+		@Override
+		protected void setFDA_DATE(Annotation annotation, String string) {
+			annotation.addFeature(fdaDateFeatureName, string);
+		}
+
+		@Override
+		protected void setFDA(Annotation annotation, String string) {
+			annotation.addFeature(fdaFeatureName, string);
+		}
+
+		@Override
+		protected void setMESH(Annotation annotation, String string) {
+			annotation.addFeature(meshFeatureName, string);
+		}
+
+		@Override
+		protected void setKEGD(Annotation annotation, String string) {
+			annotation.addFeature(kegdFeatureName, string);
+		}
+
+		@Override
+		protected void setKEGG(Annotation annotation, String string) {
+			annotation.addFeature(keggFeatureName, string);
+		}
+
+		@Override
+		protected void setHMBD(Annotation annotation, String string) {
+			annotation.addFeature(hmdbFeatureName, string);
+		}
+
+		@Override
+		protected void setDRUG(Annotation annotation, String string) {
+			annotation.addFeature(drugFeatureName, string);
+		}
+
+		@Override
+		protected void setINCH(Annotation annotation, String string) {
+			annotation.addFeature(inchFeatureName, string);
+		}
+
+		@Override
+		protected void setPUBS(Annotation annotation, String string) {
+			annotation.addFeature(pubsFeatureName, string);
+		}
+
+		@Override
+		protected void setPUBC(Annotation annotation, String string) {
+			annotation.addFeature(pubcFeatureName, string);
+		}
+
+		@Override
+		protected void setCAS(Annotation annotation, String string) {
+			annotation.addFeature(casFeatureName, string);
+		}
+
+		@Override
+		protected void setCHEB(Annotation annotation, String string) {
+			annotation.addFeature(chebFeatureName, string);
+		}
+
+		@Override
+		protected void setCHID(Annotation annotation, String string) {
+			annotation.addFeature(chidFeatureName, string);
+		}
+
+		@Override
+		protected void setType(Annotation annotation, String string) {
+			annotation.addFeature(chemTypeFeatureName, string);
+		}
+
+		@Override
+		protected void setText(Annotation annotation, String string) {
+		}
+
+		@Override
+		protected Annotation createAnnotation(Layer data, int start, int end) {
+			return new Annotation(Chemspot.this, data, start, end);
+		}
+	}
+
+	@Override
+	protected String[] addLayersToSectionFilter() {
+		return null;
+	}
+
+	@Override
+	protected String[] addFeaturesToSectionFilter() {
+		return null;
+	}
+
+	@Override
+	protected SectionResolvedObjects createResolvedObjects(ProcessingContext<Corpus> ctx) throws ResolverException {
+		return new SectionResolvedObjects(ctx, this);
+	}
+
+	@Param
+	public InputDirectory getJavaHome() {
+		return javaHome;
+	}
+
+	@Param
+	public InputDirectory getChemspotDir() {
+		return chemspotDir;
+	}
+
+	@Param(nameType=NameType.LAYER)
+	public String getTargetLayerName() {
+		return targetLayerName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getFdaDateFeatureName() {
+		return fdaDateFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getFdaFeatureName() {
+		return fdaFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getMeshFeatureName() {
+		return meshFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getKegdFeatureName() {
+		return kegdFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getKeggFeatureName() {
+		return keggFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getHmdbFeatureName() {
+		return hmdbFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getDrugFeatureName() {
+		return drugFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getInchFeatureName() {
+		return inchFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getPubsFeatureName() {
+		return pubsFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getPubcFeatureName() {
+		return pubcFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getCasFeatureName() {
+		return casFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getChebFeatureName() {
+		return chebFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getChidFeatureName() {
+		return chidFeatureName;
+	}
+
+	@Param(nameType=NameType.FEATURE)
+	public String getChemTypeFeatureName() {
+		return chemTypeFeatureName;
+	}
+
+	@Param
+	public Boolean getNoDict() {
+		return noDict;
+	}
+
+	public void setNoDict(Boolean noDict) {
+		this.noDict = noDict;
+	}
+
+	public void setJavaHome(InputDirectory javaHome) {
+		this.javaHome = javaHome;
+	}
+
+	public void setChemspotDir(InputDirectory chemspotDir) {
+		this.chemspotDir = chemspotDir;
+	}
+
+	public void setTargetLayerName(String targetLayerName) {
+		this.targetLayerName = targetLayerName;
+	}
+
+	public void setFdaDateFeatureName(String fdaDateFeatureName) {
+		this.fdaDateFeatureName = fdaDateFeatureName;
+	}
+
+	public void setFdaFeatureName(String fdaFeatureName) {
+		this.fdaFeatureName = fdaFeatureName;
+	}
+
+	public void setMeshFeatureName(String meshFeatureName) {
+		this.meshFeatureName = meshFeatureName;
+	}
+
+	public void setKegdFeatureName(String kegdFeatureName) {
+		this.kegdFeatureName = kegdFeatureName;
+	}
+
+	public void setKeggFeatureName(String keggFeatureName) {
+		this.keggFeatureName = keggFeatureName;
+	}
+
+	public void setHmdbFeatureName(String hmdbFeatureName) {
+		this.hmdbFeatureName = hmdbFeatureName;
+	}
+
+	public void setDrugFeatureName(String drugFeatureName) {
+		this.drugFeatureName = drugFeatureName;
+	}
+
+	public void setInchFeatureName(String inchFeatureName) {
+		this.inchFeatureName = inchFeatureName;
+	}
+
+	public void setPubsFeatureName(String pubsFeatureName) {
+		this.pubsFeatureName = pubsFeatureName;
+	}
+
+	public void setPubcFeatureName(String pubcFeatureName) {
+		this.pubcFeatureName = pubcFeatureName;
+	}
+
+	public void setCasFeatureName(String casFeatureName) {
+		this.casFeatureName = casFeatureName;
+	}
+
+	public void setChebFeatureName(String chebFeatureName) {
+		this.chebFeatureName = chebFeatureName;
+	}
+
+	public void setChidFeatureName(String chidFeatureName) {
+		this.chidFeatureName = chidFeatureName;
+	}
+
+	public void setChemTypeFeatureName(String chemTypeFeatureName) {
+		this.chemTypeFeatureName = chemTypeFeatureName;
+	}
+}
---
+++ alvisnlp-bibliome/src/main/java/org/bibliome/alvisnlp/modules/chemspot/ChemspotFileLines.java
@@ -0,0 +1,52 @@
+package org.bibliome.alvisnlp.modules.chemspot;
+
+import java.util.List;
+
+import org.bibliome.util.filelines.FileLines;
+import org.bibliome.util.filelines.InvalidFileLineEntry;
+
+public abstract class ChemspotFileLines<D,A> extends FileLines<D> {
+
+	@Override
+	public void processEntry(D data, int lineno, List<String> entry) throws InvalidFileLineEntry {
+		int begin = Integer.parseInt(entry.get(1));
+		if (begin == -1) {
+			return;
+		}
+		int end = Integer.parseInt(entry.get(2));
+		A annotation = createAnnotation(data, begin, end);
+		setText(annotation, entry.get(3));
+		setType(annotation, entry.get(4));
+		setCHID(annotation, entry.get(5));
+		setCHEB(annotation, entry.get(6));
+		setCAS(annotation, entry.get(7));
+		setPUBC(annotation, entry.get(8));
+		setPUBS(annotation, entry.get(9));
+		setINCH(annotation, entry.get(10));
+		setDRUG(annotation, entry.get(11));
+		setHMBD(annotation, entry.get(12));
+		setKEGG(annotation, entry.get(13));
+		setKEGD(annotation, entry.get(14));
+		setMESH(annotation, entry.get(15));
+		setFDA(annotation, entry.get(16));
+		setFDA_DATE(annotation, entry.get(17));
+	}
+	
+	protected abstract void setFDA_DATE(A annotation, String string);
+
+	protected abstract void setFDA(A annotation, String string);
+	protected abstract void setMESH(A annotation, String string);
+	protected abstract void setKEGD(A annotation, String string);
+	protected abstract void setKEGG(A annotation, String string);
+	protected abstract void setHMBD(A annotation, String string);
+	protected abstract void setDRUG(A annotation, String string);
+	protected abstract void setINCH(A annotation, String string);
+	protected abstract void setPUBS(A annotation, String string);
+	protected abstract void setPUBC(A annotation, String string);
+	protected abstract void setCAS(A annotation, String string);
+	protected abstract void setCHEB(A annotation, String string);
+	protected abstract void setCHID(A annotation, String string);
+	protected abstract void setType(A annotation, String string);
+	protected abstract void setText(A annotation, String string);
+	protected abstract A createAnnotation(D data, int start, int end);
+}
