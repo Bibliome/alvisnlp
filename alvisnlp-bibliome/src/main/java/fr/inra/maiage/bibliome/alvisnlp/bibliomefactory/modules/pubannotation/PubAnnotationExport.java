c4e9d043903cc287424c2474ec44c8d5f03d67cb
Robert Bossy <Robert.Bossy@jouy.inra.fr>
2018-01-12 18:15:52 +0100

    added parameters sourcedb sourceid divid

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/pubannotation/PubAnnotationExport.java
+++ alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/pubannotation/PubAnnotationExport.java
@@ -6,15 +6,17 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.logging.Logger;
 
-import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.SectionModule;
-import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.SectionModule.SectionResolvedObjects;
-import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.pubannotation.PubAnnotationExport.PubAnnotationExportResolvedObjects;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.SectionModule;
+import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.SectionModule.SectionResolvedObjects;
+import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.pubannotation.PubAnnotationExport.PubAnnotationExportResolvedObjects;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Corpus;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Section;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.EvaluationContext;
+import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.Evaluator;
+import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.Expression;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.LibraryResolver;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.ResolverException;
 import fr.inra.maiage.bibliome.alvisnlp.core.module.ModuleException;
@@ -30,6 +32,9 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 	private OutputFile outFile;
 	private DenotationSpecification[] denotations = { new DenotationSpecification() };
 	private RelationSpecification[] relations = { new RelationSpecification() };
+	private Expression sourcedb;
+	private Expression sourceid;
+	private Expression divid;
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -75,14 +80,20 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 	}
 	
 	public static class PubAnnotationExportResolvedObjects extends SectionResolvedObjects {
-		private DenotationSpecification.Resolved[] denotations;
-		private RelationSpecification.Resolved[] relations;
+		private final DenotationSpecification.Resolved[] denotations;
+		private final RelationSpecification.Resolved[] relations;
+		private final Evaluator sourcedb;
+		private final Evaluator sourceid;
+		private final Evaluator divid;
 		
 		private PubAnnotationExportResolvedObjects(ProcessingContext<Corpus> ctx, PubAnnotationExport module) throws ResolverException {
 			super(ctx, module);
 			LibraryResolver resolver = module.getLibraryResolver(ctx);
 			this.denotations = resolver.resolveArray(module.denotations, DenotationSpecification.Resolved.class);
 			this.relations = resolver.resolveArray(module.relations, RelationSpecification.Resolved.class);
+			this.sourcedb = resolver.resolveNullable(module.sourcedb);
+			this.sourceid = resolver.resolveNullable(module.sourceid);
+			this.divid = resolver.resolveNullable(module.divid);
 		}
 
 		@Override
@@ -90,6 +101,9 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 			super.collectUsedNames(nameUsage, defaultType);
 			nameUsage.collectUsedNamesArray(denotations, defaultType);
 			nameUsage.collectUsedNamesArray(relations, defaultType);
+			nameUsage.collectUsedNamesNullable(sourcedb, defaultType);
+			nameUsage.collectUsedNamesNullable(sourceid, defaultType);
+			nameUsage.collectUsedNamesNullable(divid, defaultType);
 		}
 		
 		@SuppressWarnings("unchecked")
@@ -98,6 +112,15 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 			result.put("text", sec.getContents());
 			result.put("denotations", getDenotations(ctx, sec));
 			result.put("relations", getRelations(ctx, sec));
+			if (sourcedb != null) {
+				result.put("sourcedb", sourcedb.evaluateString(ctx, sec));
+			}
+			if (sourceid != null) {
+				result.put("sourceid", sourceid.evaluateString(ctx, sec));
+			}
+			if (divid != null) {
+				result.put("divid", divid.evaluateInt(ctx, sec));
+			}
 			return result;
 		}
 		
@@ -133,6 +156,33 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 		return relations;
 	}
 
+	@Param(mandatory=false)
+	public Expression getSourcedb() {
+		return sourcedb;
+	}
+
+	@Param(mandatory=false)
+	public Expression getSourceid() {
+		return sourceid;
+	}
+
+	@Param(mandatory=false)
+	public Expression getDivid() {
+		return divid;
+	}
+
+	public void setSourcedb(Expression sourcedb) {
+		this.sourcedb = sourcedb;
+	}
+
+	public void setSourceid(Expression sourceid) {
+		this.sourceid = sourceid;
+	}
+
+	public void setDivid(Expression divid) {
+		this.divid = divid;
+	}
+
 	public void setOutFile(OutputFile outFile) {
 		this.outFile = outFile;
 	}
@@ -144,6 +194,4 @@ public class PubAnnotationExport extends SectionModule<PubAnnotationExportResolv
 	public void setRelations(RelationSpecification[] relations) {
 		this.relations = relations;
 	}
-	
-	
 }
