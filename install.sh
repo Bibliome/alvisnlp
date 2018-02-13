aed20f6af8feef776f8544d9898469458644dd53
Robert Bossy <Robert.Bossy@inra.fr>
2018-02-13 08:46:01 +0100

    removed make-java-launcher.sh, code incorporated in install.sh

--- install.sh
+++ install.sh
@@ -50,5 +50,46 @@ then
     cp -f -u "share/default-options.txt" "$SHARE_DIR/default-options.txt"
 fi
 
-./make-java-launcher.sh $INSTALL_DIR
+read -r -d '' TEMPLATE <<'EOF'
+
+#!/bin/bash
+
+# qsub options
+#$ -S /bin/bash
+#$ -V
+#$ -cwd
+
+ulimit -c 0
+
+export CLASSPATH="__INSTALL_DIR__/lib/*:$CLASSPATH"
+
+JVMOPTS=
+OPTERR=0
+while getopts J: o; do
+    case "$o" in
+	J) JVMOPTS="$JVMOPTS $OPTARG";;
+	?) break;;
+    esac
+done
+shift $(($OPTIND-1))
+
+OPTS=""
+if [ -f "__INSTALL_DIR__/share/default-param-values.xml" ]
+then
+    OPTS="$OPTS ""-defaultParamValuesFile __INSTALL_DIR__/share/default-param-values.xml"
+fi
+if [ -f "__INSTALL_DIR__/share/default-options.txt" ]
+then
+    OPTS="$OPTS "$(cat __INSTALL_DIR__/share/default-options.txt)
+fi
+
+cmd="java $JVMOPTS fr.inra.maiage.bibliome.alvisnlp.core.app.cli.AlvisNLP ""$OPTS ""$@"
+#echo $CLASSPATH
+#echo $cmd
+$cmd
+EOF
+
+BIN_FILE="$BIN_DIR/alvisnlp"
+sed -e "s,__INSTALL_DIR__,$INSTALL_DIR," <<<"$TEMPLATE" >"$BIN_FILE"
+chmod +x "$BIN_FILE"
 
