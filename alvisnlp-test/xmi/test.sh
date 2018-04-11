fe9ced90d82c1d9234bd623e2e0a03b1cba99308
Robert Bossy <Robert.Bossy@inra.fr>
2018-04-11 18:28:15 +0200

    added DKPro import test

--- alvisnlp-test/xmi/test.sh
+++ alvisnlp-test/xmi/test.sh
@@ -3,3 +3,5 @@ check-file export
 check-file-sorted export-features.txt
 run-alvisnlp import.plan
 check-file-sorted import-features.txt
+run-alvisnlp import-dkpro.plan
+check-file-sorted import-dkpro-features.txt
