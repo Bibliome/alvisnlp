da07a9e507222f963a4d7f5e7ebde00b937088b6
Robert Bossy <Robert.Bossy@inra.fr>
2019-09-18 10:37:29 +0200

    simpler treeview

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/http/api/ElementToTreeviewChildrenJSONConverter.java
+++ alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/http/api/ElementToTreeviewChildrenJSONConverter.java
@@ -8,28 +8,23 @@ import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Corpus;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Document;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Element;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.ElementVisitor;
+import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Layer;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Relation;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Section;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Tuple;
+import fr.inra.maiage.bibliome.util.Iterators;
 
 public enum ElementToTreeviewChildrenJSONConverter implements ElementVisitor<JSONArray,JSONArray> {
 	INSTANCE;
-	
+
 	@SuppressWarnings("unchecked")
-	private static JSONArray addChild(JSONArray children, String prefix, Element elt, String text, String icon) {
+	private static JSONArray addFeaturesChild(JSONArray children, Element elt) {
 		JSONObject child = new JSONObject();
-		child.put("id", prefix + "-" + elt.getStringId());
-		child.put("text", text);
-		child.put("hasChildren", true);
+		child.put("id", "features-" + elt.getStringId());
+		child.put("text", "<span class=\"title-node features-node\">Features</span>");
+		child.put("hasChildren", !elt.isFeatureless());
 		children.add(child);
 		return children;
-	}
-	
-	private static JSONArray addFeaturesChild(JSONArray children, Element elt) {
-		if (elt.isFeatureless()) {
-			return children;
-		}
-		return addChild(children, "features", elt, "<span class=\"title-node features-node\">Features</span>", null);
 	}
 
 	@Override
@@ -37,50 +32,66 @@ public enum ElementToTreeviewChildrenJSONConverter implements ElementVisitor<JSO
 		return addFeaturesChild(param, a);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONArray visit(Corpus corpus, JSONArray param) {
 		addFeaturesChild(param, corpus);
-		if (corpus.documentIterator().hasNext()) {
-			addChild(param, "documents", corpus, "<span class=\"title-node documents-node\">Documents</span>", null);
+		for (Document doc : Iterators.loop(corpus.documentIterator())) {
+			JSONObject jDoc = doc.accept(ElementToTreeviewJSONConverter.INSTANCE, null);
+			param.add(jDoc);
 		}
 		return param;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONArray visit(Document doc, JSONArray param) {
 		addFeaturesChild(param, doc);
-		if (doc.sectionIterator().hasNext()) {
-			addChild(param, "sections", doc, "<span class=\"title-node sections-node\">Sections</span>", null);
+		for (Section sec : Iterators.loop(doc.sectionIterator())) {
+			JSONObject jSec = sec.accept(ElementToTreeviewJSONConverter.INSTANCE, null);
+			param.add(jSec);
 		}
 		return param;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONArray visit(Relation rel, JSONArray param) {
 		addFeaturesChild(param, rel);
-		if (!rel.getTuples().isEmpty()) {
-			addChild(param, "tuples", rel, "<span class=\"title-node tuples-node\">Tuples</span>", null);
+		for (Tuple t : rel.getTuples()) {
+			JSONObject jT = t.accept(ElementToTreeviewJSONConverter.INSTANCE, null);
+			param.add(jT);
 		}
 		return param;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONArray visit(Section sec, JSONArray param) {
 		addFeaturesChild(param, sec);
-		if (!sec.getAllAnnotations().isEmpty()) {
-			addChild(param, "layers", sec, "<span class=\"title-node layers-node\">Layers</span>", null);
+		for (Layer layer : sec.getAllLayers()) {
+			String name = layer.getName();
+			JSONObject jLayer = new JSONObject();
+			jLayer.put("id", String.format("annotations-%s-%s", sec.getStringId(), name));
+			jLayer.put("text", String.format("<span class=\"layer-node\">%s</span>", name));
+			jLayer.put("hasChildren", !layer.isEmpty());
+			param.add(jLayer);
 		}
-		if (!sec.getAllRelations().isEmpty()) {
-			addChild(param, "relations", sec, "<span class=\"title-node relations-node\">Relations</span>", null);
+		for (Relation rel : sec.getAllRelations()) {
+			JSONObject jRel = rel.accept(ElementToTreeviewJSONConverter.INSTANCE, null);
+			param.add(jRel);
 		}
 		return param;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONArray visit(Tuple t, JSONArray param) {
 		addFeaturesChild(param, t);
-		if (t.getArity() > 0) {
-			addChild(param, "arguments", t, "<span class=\"title-node arguments-node\">Arguments</span>", null);
+		for (String role : t.getRoles()) {
+			Element arg = t.getArgument(role);
+			JSONObject jArg = arg.accept(ElementToTreeviewJSONConverter.INSTANCE, String.format("<span class=\"argument-role\">%s</span>", role));
+			param.add(jArg);
 		}
 		return param;
 	}
