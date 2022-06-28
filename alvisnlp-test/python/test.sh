0637fa5ad4e6c06bd4e6283a301889583bdcdc22
Robert Bossy <Robert.Bossy@inra.fr>
2022-06-28 10:08:59 +0200

    updated tests for PythonScript and added test for Stanza

--- alvisnlp-test/python/test.sh
+++ alvisnlp-test/python/test.sh
@@ -2,3 +2,5 @@ run-alvisnlp simple.plan
 check-file-sorted features-before.txt
 check-file-sorted features-after.txt
 check-file-sorted features-update.txt
+run-alvisnlp stanza.plan
+check-file-sorted features-stanza.txt
