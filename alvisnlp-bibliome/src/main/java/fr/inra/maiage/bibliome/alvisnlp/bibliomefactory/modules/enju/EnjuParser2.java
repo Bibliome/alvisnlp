ee99804c386c28ea5b9e0dce15855bf35f3a79b5
Robert Bossy <Robert.Bossy@inra.fr>
2022-10-03 13:46:49 +0200

    dropped module EnjuParser2

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/enju/EnjuParser2.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.enju;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=EnjuParser.class)
-public abstract class EnjuParser2 extends EnjuParser {
-
-}
