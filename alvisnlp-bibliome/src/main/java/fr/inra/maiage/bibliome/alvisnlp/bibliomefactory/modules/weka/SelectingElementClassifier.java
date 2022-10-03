438c72a32fd2088a4c462555317a59a29b4568b4
Robert Bossy <Robert.Bossy@inra.fr>
2022-10-03 13:57:40 +0200

    dropped obsolete weka modules

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/weka/SelectingElementClassifier.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.weka;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=WekaSelectAttributes.class)
-public class SelectingElementClassifier extends WekaSelectAttributes {
-
-}
--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/weka/TaggingElementClassifier.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.weka;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=WekaPredict.class)
-public class TaggingElementClassifier extends WekaPredict {
-
-}
