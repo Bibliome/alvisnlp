5b4dcc19977423986bc6f188465665fe19fe1ec3
Robert Bossy <Robert.Bossy@inra.fr>
2022-10-03 13:52:51 +0200

    dropped module FileMapper2

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/mapper/FileMapper2.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.mapper;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=FileMapper.class)
-public class FileMapper2 extends FileMapper {
-
-}
