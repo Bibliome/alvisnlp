1d28b4c91d86c88f475e907397c45203ba9ef9cc
Robert Bossy <Robert.Bossy@inra.fr>
2022-10-03 13:55:11 +0200

    dropped modules ElementProjector2 SimpleProjector2

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/trie/SimpleProjector2.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.trie;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=TabularProjector.class)
-public abstract class SimpleProjector2 extends TabularProjector {
-
-}
