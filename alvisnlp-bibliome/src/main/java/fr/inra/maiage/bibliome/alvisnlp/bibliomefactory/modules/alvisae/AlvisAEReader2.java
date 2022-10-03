6eaa972795ee270278b8c6f84e7ca81f52b15a69
Robert Bossy <Robert.Bossy@inra.fr>
2022-10-03 13:44:36 +0200

    dropped modules AlvisAEReader2 ExportCadixeJSON

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/alvisae/AlvisAEReader2.java
+++
@@ -1,8 +0,0 @@
-package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.alvisae;
-
-import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
-
-@AlvisNLPModule(obsoleteUseInstead=AlvisAEReader.class)
-public abstract class AlvisAEReader2 extends AlvisAEReader {
-
-}
