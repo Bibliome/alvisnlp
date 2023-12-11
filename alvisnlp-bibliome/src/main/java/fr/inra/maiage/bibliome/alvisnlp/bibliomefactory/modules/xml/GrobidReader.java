dc4090e0f5c37c10b10a32892535d446796616db
Robert Bossy <Robert.Bossy@inra.fr>
2023-12-11 16:07:05 +0100

    Use grobid main class
    fullExtract is not documented in Java API

--- alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/xml/GrobidReader.java
+++ alvisnlp-bibliome/src/main/java/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/modules/xml/GrobidReader.java
@@ -1,12 +1,12 @@
 package fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.xml;
 
-import java.util.Arrays;
+import java.io.File;
 
 import org.grobid.core.engines.ProcessEngine;
-import org.grobid.core.main.GrobidHomeFinder;
-import org.grobid.core.utilities.GrobidProperties;
+import org.grobid.core.main.batch.GrobidMain;
+import org.grobid.core.main.batch.GrobidMainArgs;
+import org.grobid.core.utilities.Utilities;
 
-import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.CorpusModule;
 import fr.inra.maiage.bibliome.alvisnlp.bibliomefactory.modules.ResolvedObjects;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.Corpus;
 import fr.inra.maiage.bibliome.alvisnlp.core.corpus.expressions.ResolverException;
@@ -15,28 +15,45 @@ import fr.inra.maiage.bibliome.alvisnlp.core.module.ProcessingContext;
 import fr.inra.maiage.bibliome.alvisnlp.core.module.ProcessingException;
 import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.AlvisNLPModule;
 import fr.inra.maiage.bibliome.alvisnlp.core.module.lib.Param;
+import fr.inra.maiage.bibliome.alvisnlp.core.module.types.Mapping;
 import fr.inra.maiage.bibliome.util.files.InputDirectory;
+import fr.inra.maiage.bibliome.util.streams.SourceStream;
+import fr.inra.maiage.bibliome.util.streams.StreamFactory;
 
 @AlvisNLPModule(beta = true)
-public class GrobidReader extends CorpusModule<ResolvedObjects> {
+public abstract class GrobidReader extends AbstractXMLReader<ResolvedObjects> {
 	private InputDirectory grobidHome;
+	private InputDirectory source;
+	private String xmlDirPath;
 	
 	@Override
 	public void process(ProcessingContext ctx, Corpus corpus) throws ModuleException {
-		GrobidHomeFinder grobidHomeFinder = new GrobidHomeFinder(Arrays.asList(grobidHome.getAbsolutePath()));
-		grobidHomeFinder.findGrobidHomeOrFail();
-		GrobidProperties.getInstance(grobidHomeFinder);
-        try (ProcessEngine processEngine = new ProcessEngine()) {
-        	processEngine.processFullText(null); /* XXX
-        	https://grobid.readthedocs.io/en/latest/Grobid-batch/
-        	https://grobid.readthedocs.io/en/latest/Grobid-java-library/
-        	https://grobid.github.io/grobid-core/index.html
-        	https://grobid.readthedocs.io/en/latest/Consolidation/
-        	 */
-        }
-        catch (Exception e) {
-        	throw new ProcessingException(e);
+		try {
+			File tmpDir = getTempDir(ctx);
+			xmlDirPath = tmpDir.getAbsolutePath();
+			GrobidStub.main(buildGrobidCommandline());
+			processDocuments(ctx, corpus);
 		}
+		catch (Exception e) {
+			throw new ProcessingException(e);
+		}
+	}
+	
+	private String[] buildGrobidCommandline() {
+		return new String[] {
+				"-gH",
+				grobidHome.getAbsolutePath(),
+				"-dIn",
+				source.getAbsolutePath(),
+				"-dOut",
+				xmlDirPath,
+				"-r",
+				"-ignoreAssets",
+				"-addElementId",
+				"-segmentSentences",
+				"-exe",
+				"processFullText"
+			};
 	}
 
 	@Override
@@ -44,9 +61,44 @@ public class GrobidReader extends CorpusModule<ResolvedObjects> {
 		return new ResolvedObjects(ctx, this);
 	}
 
+	@Override
+	public SourceStream getXslTransform() {
+		//return new StreamFactory().getFileSourceStream("/home/rbossy/code/alvisnlp/alvisnlp-bibliome/src/main/resources/fr/inra/maiage/bibliome/alvisnlp/bibliomefactory/resources/XMLReader/grobid-tei2alvisnlp.xslt");
+		return new StreamFactory().getFileSourceStream("/home/rbossy/code/pesv-tm/TEI/trafilatura-tei2alvisnlp.xslt");
+	}
+
+	@Override
+	public Mapping getStringParams() {
+		return new Mapping();
+	}
+
+	@Override
+	public SourceStream getXMLSource() {
+		return new StreamFactory().getDirectorySourceStream(xmlDirPath);
+	}
+
+	@Override
+	public Boolean getHtml() {
+		return false;
+	}
+
+	@Override
+	public Boolean getRawTagNames() {
+		return true;
+	}
+
 	@Param
 	public InputDirectory getGrobidHome() {
 		return grobidHome;
+	}
+
+	@Param
+	public InputDirectory getSource() {
+		return source;
+	}
+
+	public void setSource(InputDirectory source) {
+		this.source = source;
 	}
 
 	public void setGrobidHome(InputDirectory grobidHome) {
